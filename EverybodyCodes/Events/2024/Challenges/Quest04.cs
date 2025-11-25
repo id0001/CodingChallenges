1db8840ad1bb5615719aa38f8863a5822b320233
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2025-11-25 17:07:09 +0100

    Quest 4

--- EverybodyCodes/Events/2024/Challenges/Quest04.cs
+++ EverybodyCodes/Events/2024/Challenges/Quest04.cs
@@ -3,21 +3,34 @@ using CodingChallenge.Utilities.Extensions;
 
 namespace EverybodyCodes2024.Challenges;
 
-//[Challenge(4)]
+[Challenge(4)]
 public class Quest04
 {
-    //[Part(1)]
-    //public string Part1(string input)
-    //{
-    //}
+    [Part(1, "87")]
+    public string Part1(string input)
+    {
+        var nails = input.Lines().As<int>().ToArray();
+        var min = nails.Min();
+        return nails.Sum(x => x - min).ToString();
+    }
 
-	//[Part(2)]
-    //public string Part2(string input)
-    //{
-    //}
-	
-	//[Part(3)]
-    //public string Part3(string input)
-    //{
-    //}
+    [Part(2, "894606")]
+    public string Part2(string input)
+    {
+        var nails = input.Lines().As<int>().ToArray();
+        return Calculate(nails, nails.Min()).ToString();
+    }
+
+    [Part(3, "120168130")]
+    public string Part3(string input)
+    {
+        var nails = input.Lines().As<int>().ToArray();
+
+        var sorted = nails.Order().ToList();
+        var median = sorted[sorted.Count / 2];
+        return Calculate(nails, median).ToString();
+        
+    }
+
+    private static long Calculate(IEnumerable<int> nails, int height) => nails.Sum(x => (long)Math.Abs(x - height));
 }
