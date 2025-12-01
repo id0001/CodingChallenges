0a2b47ae7c3d976eb6079924e4b57cdc5904976b
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2025-12-01 06:20:09 +0100

    2025 day 1

--- AdventOfCode/Years/2025/Challenges/Challenge01.cs
+++ AdventOfCode/Years/2025/Challenges/Challenge01.cs
@@ -6,14 +6,51 @@ namespace AdventOfCode2025.Challenges;
 [Challenge(1)]
 public class Challenge01
 {
-    [Part(1)]
+    [Part(1, "1147")]
     public string Part1(string input)
     {
-        return string.Empty;
+        int sum = 0;
+        int value = 50;
+
+        foreach (var line in input.Lines())
+        {
+            var d = line[0];
+            var v = line[1..].As<int>();
+
+            value = d switch
+            {
+                'L' => (value - v).Mod(100),
+                'R' => (value + v).Mod(100),
+                _ => value
+            };
+
+            if (value == 0)
+                sum++;
+        }
+
+        return sum.ToString();
     }
 
-    // [Part(2)]
-    // public string Part2(string input)
-    // {
-    // }
+    [Part(2, "6789")]
+    public string Part2(string input)
+    {
+        int sum = 0;
+        int value = 50;
+
+        foreach (var line in input.Lines())
+        {
+            var d = line[0];
+            var v = line[1..].As<int>();
+
+            for (var i = 0; i < v; i++)
+            {
+                value = (value + (d == 'R' ? 1 : -1)).Mod(100);
+
+                if (value == 0)
+                    sum++;
+            }
+        }
+
+        return sum.ToString();
+    }
 }
