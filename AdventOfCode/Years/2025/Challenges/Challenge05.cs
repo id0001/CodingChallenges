2e3b04cf405cfe3c131b000ed3be6cf2eb350108
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2025-12-05 09:48:38 +0100

    2025 day 5

--- AdventOfCode/Years/2025/Challenges/Challenge05.cs
+++ AdventOfCode/Years/2025/Challenges/Challenge05.cs
@@ -3,16 +3,49 @@ using CodingChallenge.Utilities.Extensions;
 
 namespace AdventOfCode2025.Challenges;
 
-//[Challenge(5)]
+[Challenge(5)]
 public class Challenge05
 {
-    // [Part(1)]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "511")]
+    public string Part1(string input)
+    {
+        var (fresh, ingredients) = ParseInput(input);
 
-    // [Part(2)]
-    // public string Part2(string input)
-    // {
-    // }
+        return ingredients
+            .Where(i => fresh.Any(r => r.IsInRange(i)))
+            .Count()
+            .ToString();
+    }
+
+    [Part(2, "350939902751909")]
+    public string Part2(string input)
+    {
+        var (fresh, _) = ParseInput(input);
+
+        return fresh
+            .MergeBy(IngredientRange.HasOverlap, IngredientRange.Merge)
+            .Sum(r => r.Length)
+            .ToString();
+    }
+
+    private static (IEnumerable<IngredientRange> FreshRanges, long[] Ingredients) ParseInput(string input)
+    {
+        return input
+            .Paragraphs()
+            .Transform(paragraphs => (
+                paragraphs.First().Lines(line => line.SplitBy<long>("-").Transform(r => new IngredientRange(r.First(), r.Second()))),
+                paragraphs.Second().Lines().Select(line => line.As<long>()).ToArray()));
+    }
+
+    private record IngredientRange(long Start, long End)
+    {
+        public long Length = (End + 1 - Start);
+
+        public bool IsInRange(long value) => value >= Start && value < Start + Length;
+
+        public static IngredientRange Merge(IngredientRange a, IngredientRange b) 
+            => new IngredientRange(Math.Min(a.Start, b.Start), Math.Max(a.End, b.End));
+
+        public static bool HasOverlap(IngredientRange a, IngredientRange b) => b.End >= a.Start && b.Start <= a.End;
+    }
 }
---
+++ CodingChallenge.Utilities/Extensions/Linq/EnumerableExtensions.MergeBy.cs
@@ -0,0 +1,46 @@
+﻿namespace CodingChallenge.Utilities.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        extension<T>(IEnumerable<T> source)
+        {
+            public IEnumerable<T> MergeBy(Func<T, T, bool> shouldMerge, Func<T, T, T> merge)
+            {
+                ArgumentNullException.ThrowIfNull(source);
+                ArgumentNullException.ThrowIfNull(shouldMerge);
+                ArgumentNullException.ThrowIfNull(merge);
+
+                var results = new List<T>();
+
+                int count = 0;
+                do
+                {
+                    count = 0;
+                    results = [];
+                    foreach (var item in source)
+                    {
+                        var merged = false;
+                        for (var i = 0; i < results.Count; i++)
+                        {
+                            if (shouldMerge(item, results[i]))
+                            {
+                                results[i] = merge(item, results[i]);
+                                merged = true;
+                                break;
+                            }
+                        }
+
+                        if (!merged)
+                            results.Add(item);
+
+                        count++;
+                    }
+
+                    source = results;
+                }
+                while (results.Count != count);
+                return results;
+            }
+        }
+    }
+}
