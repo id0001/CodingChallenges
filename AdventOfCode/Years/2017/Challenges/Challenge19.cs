dd40853bbb9e94bcdbcfb1a5280281af80f78502
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2025-12-22 09:58:54 +0100

    2017 19 - 20

--- AdventOfCode/Years/2017/Challenges/Challenge19.cs
+++ AdventOfCode/Years/2017/Challenges/Challenge19.cs
@@ -1,18 +1,60 @@
+using CodingChallenge.Utilities;
 using CodingChallenge.Utilities.Attributes;
 using CodingChallenge.Utilities.Extensions;
+using System.Text;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(19)]
+[Challenge(19)]
 public class Challenge19
 {
-    // [Part(1, "QPRYCIOLU")]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "QPRYCIOLU")]
+    public string Part1(string input)
+    {
+        var grid = input.ToGrid();
+        var start = new Point2(Array.IndexOf(grid.Row(0).ToArray(), '|'), 0);
+        var (_, result) = FollowToEnd(grid, start);
 
-    // [Part(2, "16162")]
-    // public string Part2(string input)
-    // {
-    // }
+        return result;
+    }
+
+    [Part(2, "16162")]
+    public string Part2(string input)
+    {
+        var grid = input.ToGrid();
+        var start = new Point2(Array.IndexOf(grid.Row(0).ToArray(), '|'), 0);
+        var (steps, _) = FollowToEnd(grid, start);
+
+        return steps.ToString();
+    }
+
+    private static (int Steps, string Result) FollowToEnd(Grid2<char> grid, Point2 start)
+    {
+        var pose = new Pose2(start, Face.Down);
+        var steps = 0;
+        var result = new StringBuilder();
+
+        while (grid.Bounds.Contains(pose.Position) && grid[pose.Position] != ' ')
+        {
+            switch (grid[pose.Position])
+            {
+                case '+':
+                    pose = ChangeDirection(grid, pose);
+                    break;
+                case var c when c is not '|' and not '-':
+                    result.Append(c);
+                    break;
+                default:
+                    break;
+            }
+
+            pose = pose.Step();
+            steps++;
+        }
+
+        return (steps, result.ToString());
+    }
+
+    private static Pose2 ChangeDirection(Grid2<char> grid, Pose2 pose)
+        => grid.Bounds.Contains(pose.Left) && grid[pose.Left] != ' ' ? pose.TurnLeft() : pose.TurnRight();
 }
--- AdventOfCode/Years/2017/Challenges/Challenge20.cs
+++ AdventOfCode/Years/2017/Challenges/Challenge20.cs
@@ -1,18 +1,65 @@
+using CodingChallenge.Utilities;
 using CodingChallenge.Utilities.Attributes;
 using CodingChallenge.Utilities.Extensions;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(20)]
+[Challenge(20)]
 public class Challenge20
 {
-    // [Part(1, "161")]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "161")]
+    public string Part1(string input)
+    {
+        var list = input.Lines(ParseLine).ToArray();
 
-    // [Part(2, "438")]
-    // public string Part2(string input)
-    // {
-    // }
+        for (var id = 0; id < list.Length; id++)
+            list[id] = list[id].Update(1000);
+
+        return list.Index().MinBy(x => Point3.ManhattanDistance(Point3.Zero, x.Item.Position)).Index.ToString();
+    }
+
+    [Part(2, "438")]
+    public string Part2(string input)
+    {
+        var list = input.Lines(ParseLine).ToArray();
+
+        for(var i = 0; i < 1000; i++)
+        {
+            list = list
+                .Select(x => x.Update())
+                .GroupBy(p => p.Position)
+                .Where(g => g.Count() == 1)
+                .SelectMany(g => g)
+                .ToArray();
+        }
+
+        return list.Length.ToString();
+    }
+
+    private PosVelAcc ParseLine(string line) => line
+        .Extract(@"p=<(.+)>, v=<(.+)>, a=<(.+)>")
+        .Select(ToPoint)
+        .Transform(x => new PosVelAcc(x.First(), x.Second(), x.Third()));
+
+
+    private static Point3 ToPoint(string value) => value.SplitBy<int>(",").Transform(x => new Point3(x.First(), x.Second(), x.Third()));
+
+    private record PosVelAcc(Point3 Position, Point3 Velocity, Point3 Acceleration)
+    {
+        public PosVelAcc Update()
+        {
+            var v = Velocity + Acceleration;
+            var p = Position + v;
+            return this with { Position = p, Velocity = v };
+        }
+
+        public PosVelAcc Update(int ticks)
+        {
+            var result = this;
+            for(var i = 0; i < ticks; i++)
+                result = result.Update();
+
+            return result;
+        }
+    }
 }
