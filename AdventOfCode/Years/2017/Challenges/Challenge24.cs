47f09e55e0fb94799b3db3477fb083d5a04194d7
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2026-01-14 11:16:40 +0100

    Done with 2017

--- AdventOfCode/Years/2017/Challenges/Challenge24.cs
+++ AdventOfCode/Years/2017/Challenges/Challenge24.cs
@@ -1,18 +1,59 @@
 using CodingChallenge.Utilities.Attributes;
 using CodingChallenge.Utilities.Extensions;
+using System.Collections.Immutable;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(24)]
+[Challenge(24)]
 public class Challenge24
 {
-    // [Part(1, "1906")]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "1906")]
+    public string Part1(string input)
+    {
+        var bridges = input.Lines(ParseLine).ToList();
 
-    // [Part(2, "1824")]
-    // public string Part2(string input)
-    // {
-    // }
+        return EnumerateBridges(bridges, 0, [])
+            .Max(arr => arr.Sum(b => b.Strength))
+            .ToString();
+    }
+
+    [Part(2, "1824")]
+    public string Part2(string input)
+    {
+        var bridges = input.Lines(ParseLine).ToList();
+
+        return EnumerateBridges(bridges, 0, [])
+            .OrderByDescending(arr => arr.Count)
+            .Select(arr => arr.Sum(b => b.Strength))
+            .First()
+            .ToString();
+    }
+
+    private static IEnumerable<ImmutableHashSet<Bridge>> EnumerateBridges(IEnumerable<Bridge> bridges, int pins, ImmutableHashSet<Bridge> current)
+    {
+        var available = bridges.Where(b => b.IsOption(pins) && !current.Contains(b)).ToList();
+        if (available.Count == 0)
+        {
+            yield return current;
+            yield break;
+        }
+
+        foreach (var option in available)
+        {
+            foreach (var item in EnumerateBridges(bridges, option.Other(pins), current.Add(option)))
+                yield return item;
+        }
+    }
+
+    private static Bridge ParseLine(string line)
+        => line.Extract<int, int>(@"(\d+)/(\d+)").Transform(parts => new Bridge(parts.First, parts.Second));
+
+    private record Bridge(int A, int B)
+    {
+        public int Strength = A + B;
+
+        public bool IsOption(int pins) => A == pins || B == pins;
+
+        public int Other(int pins) => A == pins ? B : A;
+    }
 }
--- AdventOfCode/Years/2017/Challenges/Challenge25.cs
+++ AdventOfCode/Years/2017/Challenges/Challenge25.cs
@@ -3,16 +3,66 @@ using CodingChallenge.Utilities.Extensions;
 
 namespace AdventOfCode2017.Challenges;
 
-//[Challenge(25)]
+[Challenge(25)]
 public class Challenge25
 {
-    // [Part(1, "4387")]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "4387")]
+    public string Part1(string input)
+    {
+        var (steps, rules) = input.Paragraphs().Transform(paragraphs => 
+        (
+            paragraphs.First().Extract<int>(@"checksum after (\d+) steps.$"),
+            ParseRules(paragraphs.Skip(1))
+        ));
 
-    // [Part(2)]
-    // public string Part2(string input)
-    // {
-    // }
+        var currentState = "A";
+        var cursor = 0;
+        var tape = new Dictionary<int, int>();
+
+        for(var i = 0; i < steps; i++)
+        {
+            var rule = rules[currentState];
+            var v = tape.GetValueOrDefault(cursor, 0);
+            if(v == 0)
+            {
+                tape[cursor] = rule.WriteZero;
+                cursor += rule.MoveZero == "left" ? -1 : 1;
+                currentState = rule.NextZero;
+            }
+            else
+            {
+                tape[cursor] = rule.WriteOne;
+                cursor += rule.MoveOne == "left" ? -1 : 1;
+                currentState = rule.NextOne;
+            }
+        }
+
+        return tape.Count(kv => kv.Value == 1).ToString();
+    }
+
+    private static Dictionary<string, Rule> ParseRules(IEnumerable<string> paragraphs)
+    {
+        return paragraphs.Select(x => x.Lines().ToList().Transform(lines =>
+        {
+            var state = lines[0].Extract<string>(@"In state (.):");
+            var writeZero = lines[2].Extract<int>(@"Write the value (\d).");
+            var moveZero = lines[3].Extract<string>(@"(left|right)");
+            var nextZero = lines[4].Extract<string>(@"Continue with state (.)\.");
+
+            var writeOne = lines[6].Extract<int>(@"Write the value (\d).");
+            var moveOne = lines[7].Extract<string>(@"(left|right)");
+            var nextOne = lines[8].Extract<string>(@"Continue with state (.)\.");
+
+            return new Rule(state, writeZero, moveZero, nextZero, writeOne, moveOne, nextOne);
+        })).ToDictionary(kv => kv.State);
+    }
+
+    private record Rule(
+        string State,
+        int WriteZero,
+        string MoveZero,
+        string NextZero,
+        int WriteOne,
+        string MoveOne,
+        string NextOne);
 }
