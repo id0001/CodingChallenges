6cad34a4c4260f99bbd95ae790cfa30c7c95d324
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2026-01-15 10:38:28 +0100

    day 5

--- AdventOfCode/Years/2018/Challenges/Challenge05.cs
+++ AdventOfCode/Years/2018/Challenges/Challenge05.cs
@@ -3,16 +3,45 @@ using CodingChallenge.Utilities.Extensions;
 
 namespace AdventOfCode2018.Challenges;
 
-//[Challenge(5)]
+[Challenge(5)]
 public class Challenge05
 {
-    // [Part(1, "10132")]
-    // public string Part1(string input)
-    // {
-    // }
+    [Part(1, "10132")]
+    public string Part1(string input) => Reduce(input).Count.ToString();
 
-    // [Part(2, "4572")]
-    // public string Part2(string input)
-    // {
-    // }
+    [Part(2, "4572")]
+    public string Part2(string input) => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .Min(c => Reduce(input.Where(x => char.ToUpperInvariant(x) != c).AsString()).Count)
+        .ToString();
+
+    private static IReadOnlyCollection<char> Reduce(string input)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in input)
+        {
+            if (stack.Count == 0)
+            {
+                stack.Push(c);
+                continue;
+            }
+
+            if (IsPair(stack.Peek(), c))
+                stack.Pop();
+            else
+                stack.Push(c);
+        }
+
+        return stack;
+    }
+
+    private static bool IsPair(char a, char b)
+    {
+        var isAUpper = char.IsUpper(a);
+        var isBUpper = char.IsUpper(b);
+
+        if (!(isAUpper ^ isBUpper))
+            return false;
+
+        return char.ToUpperInvariant(a) == char.ToUpperInvariant(b);
+    }
 }
