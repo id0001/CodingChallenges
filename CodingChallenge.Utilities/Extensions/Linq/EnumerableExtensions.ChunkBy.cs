d4a0582c76bd49642c8bbdeab9045f16f7a70821
Yorrick Spierings <yorrick.spierings@othersideatwork.nl>
2026-01-15 10:05:57 +0100

    library updates

---
+++ CodingChallenge.Utilities/Extensions/Linq/EnumerableExtensions.ChunkBy.cs
@@ -0,0 +1,42 @@
+﻿namespace CodingChallenge.Utilities.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        extension<TSource>(IEnumerable<TSource> source)
+        {
+            public IEnumerable<IReadOnlyCollection<TSource>> ChunkBy(Func<TSource, bool> predicate, bool matchIsLastItem = false)
+            {
+                ArgumentNullException.ThrowIfNull(source);
+                ArgumentNullException.ThrowIfNull(predicate);
+
+                List<TSource> list = [];
+                bool skipFirst = true;
+                foreach(var item in source)
+                {
+                    if (matchIsLastItem)
+                    {
+                        list.Add(item);
+                        if(predicate(item))
+                        {
+                            yield return list;
+                            list = [];
+                        }
+
+                        continue;
+                    }
+
+                    if (predicate(item))
+                    {
+                        if (!skipFirst)
+                            yield return list;
+
+                        list = [];
+                        skipFirst = false;
+                    }
+
+                    list.Add(item);
+                }
+            }
+        }
+    }
+}
---
+++ CodingChallenge.Utilities/Extensions/Strings/StringExtensions.HammingDistance.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingChallenge.Utilities.Extensions
+{
+    public static partial class StringExtensions
+    {
+        extension(string source)
+        {
+            public int HammingDistance(string other)
+            {
+                ArgumentNullException.ThrowIfNull(source);
+                ArgumentNullException.ThrowIfNull(other);
+                if (source.Length != other.Length)
+                    throw new ArgumentException("Parameter must be of equal length.", nameof(other));
+
+                return source.Zip(other).Count(pair => pair.First != pair.Second);
+            }
+        }
+    }
+}
